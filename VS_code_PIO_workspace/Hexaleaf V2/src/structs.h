a66ceaf92f651e581a3be247660245bdd4dd7ced
Jan Tonner <tonner1@seznam.cz>
2022-02-02 13:13:20 +0100

    First upload
    added funcioning version of all my codes for driving my hexlights. Added also my vscode workspace

---
+++ VS_code_PIO_workspace/Hexaleaf V2/src/structs.h
@@ -0,0 +1,46 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+#include <stdlib.h>
+
+union FadeVector {
+    struct
+    {
+        float r;
+        float g;
+        float b;
+    };
+    struct
+    {
+        float h;
+        float s;
+        float v;
+    };
+};
+
+enum FillMode{
+    Not_fill_mode,
+    Fill_by_lines_fTop,
+    Fill_by_lines_fBottom,
+    Fill_by_lines_meet_in_midle_TB,
+    Fill_by_lines_meet_in_midle_LR,
+    Fill_by_lines_fLeft,
+    Fill_by_lines_fRight,
+    Fill_by_rotation_fBottom,
+    Fill_by_rotation_fTop,
+    Fill_mode_num
+};
+
+enum Mode{
+  Stationar,//all hex same color
+  Rotation, //all hex rotating
+  PresetAnim,
+  RotationOuter,
+  RandColorRandHex,
+  RainbowSwipeVert,
+  RainbowSwipeHorz,
+  StationarOuter,
+  RainbowOuter,
+  Mode_num
+};
+
+#endif
