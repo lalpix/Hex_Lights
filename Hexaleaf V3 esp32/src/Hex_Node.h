e59bde5d32a89ed4c072fd5f48865b19a5e3cc0f
Jan Tonner <37844751+lalpix@users.noreply.github.com>
2022-12-25 13:10:31 +0100

    Separation and cleanup

--- Hexaleaf V3 esp32/src/Hex_Node.h
+++ Hexaleaf V3 esp32/src/Hex_Node.h
@@ -1,12 +1,8 @@
-/*#include <stdint.h>
-#include <stdlib.h>
-#include <FastLED.h>
 #include "structs.h"
+#include "Defines.h"
 
-#define LEDS_IN_BOX 6
-#define NUM_BOXES 2
-
-#define LED_IN_SIDE LEDS_IN_BOX/6
+#ifndef HEX_NODE
+#define HEX_NODE
 class Hexnode
 { 
   private:
@@ -19,62 +15,26 @@ class Hexnode
     FadeVector fadeVector;
     uint8_t spin_index=0;
   public:
-      CRGB colorTo = CRGB::Black;
-      Hexnode(uint8_t index)
-      {
-         ledStart = index * LEDS_IN_BOX;
-          ledEnd = ledStart + LEDS_IN_BOX - 1;
-        }
-    int side_start_led(uint8_t n){
-      return ledStart+((n%6)*LED_IN_SIDE);
+    CRGB colorTo=CRGB::Black;
+    Hexnode(uint8_t index)
+    {   
+      ledStart = index * LEDS_IN_BOX;
+      ledEnd = ledStart + LEDS_IN_BOX - 1;
     }
-    void fill_side(CRGB clr, uint8_t n,CRGB *leds){//n is <0-5>
-      int startOffset = ledStart+(n*LED_IN_SIDE);
-      for(int i =0;i<LED_IN_SIDE;i++){
-          leds[startOffset+i] = clr;
-      }
-    }
-    void fill_vert_line(CRGB clr,uint8_t line,CRGB *leds){
-      if(line<5){ 
-          leds[ledStart+5+line] = clr;
-          leds[ledStart+4-line] = clr;    
-      }else if(line>=5){
-          leds[ledStart+5+line]=clr;
-          leds[ledStart+(34-line)]=clr;
-      }
-    }
-    void fill_hor_line(CRGB clr,uint8_t line,CRGB *leds){
-      if(line==0){
-          for(int i =0;i<5;i++){
-            leds[i+ledStart+(5*LED_IN_SIDE)] = clr;
-          }
-      }else if(line==11){
-          for(int i =0;i<5;i++){
-            leds[i+ledStart+(2*LED_IN_SIDE)] = clr;
-          }
-      }else{
-          leds[ledStart+(line-1)]=clr;
-          leds[ledEnd-LED_IN_SIDE-(line-1)]=clr;
-      }
-    }
-    void fill_hex(CRGB clr,CRGB *leds){
-      for(int i = ledStart;i<(ledStart+LEDS_IN_BOX);i++){
-            leds[i] = clr;
-        }
-      color=clr;
-    }
-    void fill_one_led(CRGB clr,uint8_t n,CRGB *leds){
-      if(n<LEDS_IN_BOX)
-        leds[ledStart+n]=color;
-    }
+    int side_start_led(uint8_t n);
+    CRGB *fill_side(CRGB clr, CRGB *leds, uint8_t n);
+    CRGB *fill_vert_line(CRGB clr, CRGB *leds, uint8_t line);
+    CRGB *fill_hor_line(CRGB clr, CRGB *leds, uint8_t line);
+    CRGB *fill_hex(CRGB clr, CRGB *leds);
+    CRGB *fill_one_led(CRGB clr, CRGB *leds, uint8_t n);
     /*
-    void compute_fade_vector()
+    CRGB* ledscompute_fade_vector()
     {
         fadeVector.r = (float)(colorTo.r - color.r) / (float)fadeTimeMs;
         fadeVector.g = (float)(colorTo.g - color.g) / (float)fadeTimeMs;
         fadeVector.b = (float)(colorTo.b - color.b) / (float)fadeTimeMs;
     }
-    void color_update()
+    CRGB* ledscolor_update()
     {
         unsigned long delta_ms = millis() - lastDrawTimer;
         lastDrawTimer = millis();
@@ -102,4 +62,5 @@ class Hexnode
       }
       return 0;
     }*/ 
-//};
+};
+#endif
---
+++ Hexaleaf V3 esp32/src/Hex_node.cpp
@@ -0,0 +1,85 @@
+#include "Hex_node.h"
+#include "Defines.h"
+
+int Hexnode::side_start_led(uint8_t n){
+  return ledStart+((n%6)*LED_IN_SIDE);
+}
+CRGB*  Hexnode::fill_side(CRGB clr,CRGB* leds, uint8_t n){//n is <0-5>
+  int startOffset = ledStart+(n*LED_IN_SIDE);
+  for(int i =0;i<LED_IN_SIDE;i++){
+      leds[startOffset+i] = clr;
+  }
+  return leds;
+}
+CRGB* Hexnode::fill_vert_line(CRGB clr,CRGB* leds,uint8_t line){
+  if(line<5){ 
+      leds[ledStart+5+line] = clr;
+      leds[ledStart+4-line] = clr;    
+  }else if(line>=5){
+      leds[ledStart+5+line]=clr;
+      leds[ledStart+(34-line)]=clr;
+  }
+  return leds;
+}
+CRGB* Hexnode::fill_hor_line(CRGB clr,CRGB* leds,uint8_t line){
+  if(line==0){
+      for(int i =0;i<5;i++){
+        leds[i+ledStart+(5*LED_IN_SIDE)] = clr;
+      }
+  }else if(line==11){
+      for(int i =0;i<5;i++){
+        leds[i+ledStart+(2*LED_IN_SIDE)] = clr;
+      }
+  }else{
+      leds[ledStart+(line-1)]=clr;
+      leds[ledEnd-LED_IN_SIDE-(line-1)]=clr;
+  }
+  return leds;
+}
+CRGB* Hexnode::fill_hex(CRGB clr,CRGB* leds){
+  for(int i = ledStart;i<(ledStart+LEDS_IN_BOX);i++){
+    leds[i] = clr;
+    }
+  return leds;
+  color=clr;
+}
+CRGB* Hexnode::fill_one_led(CRGB clr,CRGB* leds,uint8_t n){
+  if(n<LEDS_IN_BOX)
+    leds[ledStart+n]=color;
+  return leds;
+}
+    /*
+    CRGB* Hexnode::ledscompute_fade_vector()
+    {
+        fadeVector.r = (float)(colorTo.r - color.r) / (float)fadeTimeMs;
+        fadeVector.g = (float)(colorTo.g - color.g) / (float)fadeTimeMs;
+        fadeVector.b = (float)(colorTo.b - color.b) / (float)fadeTimeMs;
+    }
+    CRGB* Hexnode::ledscolor_update()
+    {
+        unsigned long delta_ms = millis() - lastDrawTimer;
+        lastDrawTimer = millis();
+        int16_t r = color.r + (fadeVector.r * delta_ms);
+        int16_t g = color.g + (fadeVector.g * delta_ms);
+        int16_t b = color.b + (fadeVector.b * delta_ms);
+        (r >= 255) ? color.r = 255 : (r <= 0) ? color.r = 0 : color.r = r;
+        (g >= 255) ? color.g = 255 : (g <= 0) ? color.g = 0 : color.g = g;
+        (b >= 255) ? color.b = 255 : (b <= 0) ? color.b = 0 : color.b = b;
+    }
+    int Hexnode::draw(){
+      if (animating)
+        {
+            color_update();
+            if (millis() - startDrawTimer >= fadeTimeMs)
+            {
+                animating = false;
+                peakTimer = millis();
+            }
+        }
+
+      for (uint16_t ledPos = ledStart; ledPos <= ledEnd; ledPos++)
+      {
+          leds[ledPos] = color;
+      }
+      return 0;
+    }*/ 
