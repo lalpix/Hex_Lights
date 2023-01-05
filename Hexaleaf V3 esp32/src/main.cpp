60de1ccd5486bb15dc9acc2602a830ec14b4dd85
Jan Tonner <37844751+lalpix@users.noreply.github.com>
2023-01-05 13:53:45 +0100

    Wifi manager and mqtt calls

--- Hexaleaf V3 esp32/src/main.cpp
+++ Hexaleaf V3 esp32/src/main.cpp
@@ -7,6 +7,9 @@
 #include <aws_iot.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
+
+#include "WiFiManager.h"
+
 #include "keys.h"
 #include "structs.h"
 #include "HEX_node.h"
@@ -15,6 +18,7 @@
 // esp_rmaker_device_t *device = esp_rmaker_device_create("Light", NULL, NULL);
 /* ----- Fastled constants ----- */
 
+const long wifiTimeout = 20000;
 const uint8_t kPinLedStrip = 22; // M5StickC grove port, yellow cable
 
 const uint8_t kNumLeds = 150;
@@ -33,39 +37,94 @@ long last_ch = 0;
 int period = 5000;
 Mode m = Stationar;
 CRGB ledStrip_[kNumLeds];
+bool power = true;
 
 WiFiClientSecure net = WiFiClientSecure();
 PubSubClient client(net);
+
 void messageHandler(char *topic, byte *payload, unsigned int length)
 {
-    Serial.print("incoming: ");
-    Serial.println(topic);
-    char msg[length+1];
-    for (int i = 0; i < length;i++){
+    Serial.print("incoming topic: ");
+    Serial.print(topic);
+    char msg[length + 1];
+    for (int i = 0; i < length; i++)
+    {
         msg[i] = (char)payload[i];
     }
     msg[length] = '\0';
-
+    Serial.print(" msg: ");
     Serial.println(msg);
-    if(strstr(topic,"Mode")){
-        hexController->change_mode((Mode)atoi((char*)payload));
-        Mode m = (Mode)atoi((char *)payload);
-        Serial.printf("Mode is now %d\n", m);
+    if (strstr(topic, "power"))
+    {
+        if (strstr((char*)payload,"off"))
+        {
+            Serial.print("power off");
+            power = false;
+        }
+        else if (strstr((char *)payload, "on"))
+        {
+            Serial.print("power on");
+            power = true;
+        }
+    }
+    if (power)
+    {
+        if (strstr(topic, "mode"))
+        {
+            Mode m = (Mode)atoi((char *)payload);
+            if (m >= Mode::Mode_num - 1)
+            {
+                Serial.printf("ERROR:not valid mode %d\n", m);
+            }
+            else
+            {
+                hexController->change_mode(m);
+                Serial.printf("Mode is now %d\n", m);
+            }
+        }
+        else if (strstr(topic, "speed"))
+        {
+            int s = atoi((char *)payload);
+            hexController->set_speed(s);
+            Serial.printf("speed is now %d\n", s);
+        }
+        else if (strstr(topic, "anim"))
+        {
+            int s = atoi((char *)payload);
+            hexController->set_pre_anim(s);
+            Serial.printf("animation is now %d\n", s);
+        }
+        else if (strstr(topic, "fade"))
+        {
+            int s = atoi((char *)payload);
+            hexController->set_fade(s);
+            Serial.printf("fade is now %d\n", s);
+        }
     }
 }
 void WifiSetup()
 {
+    Serial.println("connecting to WiFi\n");
 
+    WiFiManager wm;
+    // wm.resetSettings();
+    wm.setDebugOutput(false);
+    //WiFiManagerParameter custom_text("btn", "ender mode num here to start offline", "", 1);
+    //wm.addParameter(&custom_text);
+    leds[0] = CRGB::Red;
+    hexController->show();
     WiFi.mode(WIFI_STA);
-    WiFi.begin(WIFI_ADR, WIFI_PASS);
-    Serial.println("Connecting to Wi-Fi ");
-
-    while (WiFi.status() != WL_CONNECTED)
+    bool res = wm.autoConnect("Hexaleaf");
+    if (res)
     {
-        delay(500);
-        Serial.print("~");
+        Serial.println("WiFi connected\n");
     }
-    Serial.println();
+
+    leds[0] = CRGB::Green;
+    hexController->show();
+    leds[1] = CRGB::Red;
+    hexController->show();
+
     net.setCACert(AWS_CERT_CA);
     net.setCertificate(AWS_CERT_CRT);
     net.setPrivateKey(AWS_CERT_PRIVATE);
@@ -76,7 +135,7 @@ void WifiSetup()
     // Create a message handler
     client.setCallback(messageHandler);
 
-    Serial.println("Connecting_to_AWS_IOT");
+    Serial.println("Connecting_to_AWS_IOT\n");
 
     while (!client.connect(THINGNAME))
     {
@@ -91,30 +150,32 @@ void WifiSetup()
     }
 
     // Subscribe to a topic
-    client.subscribe("Mode");
+    client.subscribe("mode");
+    client.subscribe("speed");
+    client.subscribe("anim");
+    client.subscribe("fade");
+    client.subscribe("power");
 
-
+    leds[1] = CRGB::Green;
+    hexController->show();
     Serial.println("AWS_IoT_Connected!");
 }
 
 void setup()
 {
-    
+
     Serial.begin(115200);
     delay(2000); // power-up safety delay
     Serial.println("> Setup....");
-    WifiSetup();
-    setupI2Smic();
-    setupSpectrumAnalysis();
-    
     hexController = new Hex_controller();
     hexController->set_serial(Serial);
     hexController->init();
-
-    hexController->change_mode(AudioFreqPool);
+    WifiSetup();
+    setupI2Smic();
+    setupSpectrumAnalysis();
     hexController->set_rainbow(1);
-    hexController->set_fade(true);
-    hexController->set_speed(1);
+    hexController->set_fade(30);
+    hexController->change_mode(AudioFreqPool);
     Serial.println("Setup DONE");
 
     delay(2000);
@@ -123,136 +184,12 @@ void setup()
 void loop()
 {
     // delay( 5000 );
-    //client.loop();
-    hexController->update();
-    /*
-    float magnitudeBand[FREQ_BAND_COUNT] = {0.0f};
-
-    float magnitudeBandWeightedMax = 0.0f;
-
-    newAudioReading(magnitudeBand, &magnitudeBandWeightedMax);
-
-    // ----- Beat detection -----
-
-    // Maintain history of last three magnitude values of the bass band
-    beatHist_[kBeatDetectBandOffset][0] = beatHist_[kBeatDetectBandOffset][1];
-    beatHist_[kBeatDetectBandOffset][1] = beatHist_[kBeatDetectBandOffset][2];
-    beatHist_[kBeatDetectBandOffset][2] = magnitudeBand[kBeatDetectBandOffset] * kFreqBandAmp[kBeatDetectBandOffset] * sensitivityFactor_;
-
-    float diff1 = beatHist_[1] - beatHist_[0];
-    float diff2 = beatHist_[2] - beatHist_[1];
-    if (((diff1 >= kBeatThreshold) && (diff2 < 0)) || ((diff1 > 0) && (diff2 <= -kBeatThreshold)))
+    client.loop();
+    if (power)
     {
-        beatVisIntensity_[1] = 250;
+        hexController->update();
+    }else{
+        hexController->fill_all_hex(CRGB::Black);
+        hexController->show();
     }
-    else
-    {
-        if (beatVisIntensity_[1] >= 25)
-            beatVisIntensity_[1] -= 25;
-    }
-    // Detect magnitude peak
-   
-     // ----- Update the Led strip -----
-    CRGB tmp=CRGB::Black;
-    hexController->fill_all_hex(tmp.setHSV(250,255, beatVisIntensity_[1]));
-    */
-    /*
-    if (kNumLeds <= 2 * FREQ_BAND_COUNT + 4)
-    {
-        // Show beat detection at the beginning of the strip
-        const uint8_t numBassLeds = (kNumLeds - FREQ_BAND_COUNT) / 2;
-
-        for (int i = 0; i < numBassLeds; i++)
-        {
-            ledStrip_[i].setHSV(250, 255, beatVisIntensity_[1]);
-        }
-
-        // Show frequency intensities on the remaining Leds
-        const uint8_t colorStart = 30;
-        const uint8_t colorEnd = 210;
-        const uint8_t colorStep = (colorEnd - colorStart) / FREQ_BAND_COUNT;
-
-        for (int k = 0; k < FREQ_BAND_COUNT; k++)
-        {
-            uint8_t color = colorStart + k * colorStep;
-            uint8_t lightness = min(int(magnitudeBand[k] * kFreqBandAmp[k] * sensitivityFactor_), 255);
-
-            ledStrip_[k + numBassLeds].setHSV(color, 255, lightness);
-        }
-
-        // Show beat detection at the end of the strip
-        for (int i = numBassLeds + FREQ_BAND_COUNT; i < kNumLeds; i++)
-        {
-            ledStrip_[i].setHSV(250, 255, beatVisIntensity_[1]);
-        }
-        }
-        else
-        {
-            // Show beat detection at the beginning of the strip
-            const uint8_t numBassLeds = (kNumLeds - 2 * FREQ_BAND_COUNT) / 2;
-
-            for (int i = 0; i < numBassLeds; i++)
-            {
-                ledStrip_[i].setHSV( 250, 255, beatVisIntensity_ );
-            }
-
-            // Show frequency intensities on the remaining Leds
-            const uint8_t colorStart = 30;
-            const uint8_t colorEnd   = 210;
-            const uint8_t colorStep = (colorEnd - colorStart) / FREQ_BAND_COUNT;
-
-            for (int k = 0; k < FREQ_BAND_COUNT; k++)
-            {
-                uint8_t color = colorStart + k * colorStep;
-                uint8_t lightness = min( int(magnitudeBand[k] * kFreqBandAmp[k] * sensitivityFactor_), 255);
-
-                ledStrip_[numBassLeds + k].setHSV(color, 255, lightness);
-
-                ledStrip_[numBassLeds + 2 * FREQ_BAND_COUNT - k - 1].setHSV(color, 255, lightness);
-            }
-
-            // Show beat detection at the beginning of the strip
-            for (int i = numBassLeds + 2 * FREQ_BAND_COUNT; i < kNumLeds; i++)
-            {
-                ledStrip_[i].setHSV( 250, 255, beatVisIntensity_ );
-            }
-        }*/
-
-        FastLED.show();
-        
-    
-   /*float nf;
-
-    if (fabs(magnitudeBand[1]) < 0.001f)
-    {
-        nf = 1.0f;
-        }
-        else
-        {
-            nf = 1.0f / magnitudeBand[1];
-        }
-    Serial.printf("0:%04.2f 1:%04.2f 2:%04.2f 3:%04.2f 4:%04.2f 5:%04.2f 6:%04.2f 7:%04.2f 8:%04.2f 9:%04.2f 10:%04.2f 11:%04.2f 12:%04.2f 13:%04.2f 14:%04.2f 15:%04.2f 16:%04.2f 17:%04.2f 18:%04.2f 19:%04.2f Sum:%05.1f Sens: %04.1f \n",//t: %d
-                   magnitudeBand[0] * nf,
-                   magnitudeBand[1] * nf,
-                   magnitudeBand[2] * nf,
-                   magnitudeBand[3] * nf,
-                   magnitudeBand[4] * nf,
-                   magnitudeBand[5] * nf,
-                   magnitudeBand[6] * nf,
-                   magnitudeBand[7] * nf,
-                   magnitudeBand[8] * nf,
-                   magnitudeBand[9] * nf,
-                   magnitudeBand[10] * nf,
-                   magnitudeBand[11] * nf,
-                   magnitudeBand[12] * nf,
-                   magnitudeBand[13] * nf,
-                   magnitudeBand[14] * nf,
-                   magnitudeBand[15] * nf,
-                   magnitudeBand[16] * nf,
-                   magnitudeBand[17] * nf,
-                   magnitudeBand[18] * nf,
-                   magnitudeBand[19] * nf,
-                   0,//magnitudeSum,
-                   0//sensitivityFactor_
-                   );*/
 }
